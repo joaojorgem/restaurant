89f9815bb1c8adbc2bbaa79c75062bc77ce870e7
joaojorgem <joaojorgepatos@gmail.com>
2021-05-01 17:32:52 +0000

    change api

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-    baseURL: 'https://3333-olive-quail-cng1tzlo.ws-us04.gitpod.io/',
+    baseURL: 'https://restaurant-joao-backend.herokuapp.com/',
     timeout: 15000,
     headers: {
         'Content-Type': 'application/json',
