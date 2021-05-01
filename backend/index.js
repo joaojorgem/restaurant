92759bd753ad7c99b1074badb7e6d82d9d9cea1e
joaojorgem <joaojorgepatos@gmail.com>
2021-05-01 17:40:11 +0000

    build backend

--- backend/index.js
+++ backend/index.js
@@ -12,5 +12,5 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 require('./controllers/ReservationController')(app);
 
-const port = 3333;
+const port = process.env.PORT || 3333;
 app.listen(port, () => console.log(`Server started on port ${port}`));
